1db2da77f6901f48cd05025cda82010317d34c4c
Jason Frame <jason@onehackoranother.com>
2011-11-26 21:02:34 +0000

    platform

---
+++ app/lib/edge/platform.rb
@@ -0,0 +1,16 @@
+module Edge
+  module Platform
+    def self.name
+      "Edge CMS"
+    end
+    
+    def self.attribution
+      "&copy; 2008-2011 Jason Frame"
+    end
+    
+    def self.authors
+      [ { :name => "Jason Frame", :email => "jason@onehackoranother.com", :twitter => "jaz303" },
+        { :name => "Lewis Mackenzie", :email => "lewis@thebackwoods.co.uk", :twitter => "lewism" } ]
+    end
+  end
+end
